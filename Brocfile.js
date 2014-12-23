5bd71fa8f29cec9ea1e1223d1584bbbec92fc701
Jon Evans <jle@foraker.com>
2014-12-22 21:33:46 -0700

    Add moment and showdown dependencies
    Relevant commands:
    ```
    npm install --save-dev moment
    npm install --save-dev showdown
    ```

--- Brocfile.js
+++ Brocfile.js
@@ -17,6 +17,8 @@ var app = new EmberApp();
 // please specify an object with the list of modules as keys
 // along with the exports of each module as its value.
 
-app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+app.import('bower_components/bootstrap/dist/css/bootstrap.min.css');
+app.import('bower_components/moment/min/moment.min.js');
+app.import('bower_components/showdown/compressed/showdown.js');
 
 module.exports = app.toTree();
